7bbb822fced6da3791280ffaeb28439c48849fa0
sjeam <359824901@qq.com>
2022-12-08 16:43:23 +0800

    链接事件

--- server.php
+++ server.php
@@ -1,5 +1,5 @@
 <?php
-require_once(__DIR__ . '/server_room.php');
+// require_once(__DIR__ . '/server_room.php');
 defined('SWOOLE_SERVER') OR define('SWOOLE_SERVER','0.0.0.0');
 // 面向过程编程
 // 使用文件缓存  获取用户在线数------------------
@@ -140,3 +140,53 @@ $ws->on('close', function ($ws, $fd) {
 });
  
 $ws->start();
+
+
+
+
+
+
+
+
+
+require_once(__DIR__ . '/libs/RedisLib.php');
+// //房间号
+// function room($room){
+// 	$room_id = implode('_',$room);
+// 	return $room_id;
+// }
+
+//读取聊天记录缓存-----------------
+function getChatMessages($room){
+	$message = "message:".$room;
+	//历史聊天内容
+	$contents = RedisLib::getInstance()->lRange($message, 0, -1);
+	return $contents;
+}
+//写入聊天记录缓存
+function addChatMessages($room,$frame){
+	$message = "message:".$room;
+	//历史聊天内容
+	RedisLib::getInstance()->lPush($message, $frame->data);
+}
+
+/**
+ *   $room_id    当前房间id    
+ */
+function get_push_room($room){
+	$room_id = "room:".$room;
+	// hset(name, key, value)
+	$fds = RedisLib::getInstance()->smembers($room_id);
+	return $fds;
+}
+
+function push_room($room,$fd){
+	$room_id = "room:".$room;
+ 	$fd =RedisLib::getInstance()->sAdd($room_id,$fd);
+}
+
+function remove_fd($room,$fd){
+	$room_id = "room:".$room;
+    //用户下线了--删除元素
+	RedisLib::getInstance()->srem($room_id,$fd);
+}
